bb07ce3eba1256fef18b6dda39adede78e435fb4
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-05-16 16:02:05 -0300

    Exercício 29 feito

---
+++ ex029.py
@@ -0,0 +1,9 @@
+velocidade = float(input('Velocidade do veículo em km/h: '))
+
+if velocidade > 80:
+	multa = (velocidade - 80) * 7
+	print('Você ultrapassou o limite de 80km/h')
+	print(f'Pagará uma multa de R${multa:.2f}')
+else:
+	print(f'Velocide de {velocidade}km/h')
+	print('Sem multa')
