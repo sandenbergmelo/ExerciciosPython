b76cabb6e3d705430a4476a17e150b6ce916be15
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-05-17 14:38:02 -0300

    Exercício 33 feito

---
+++ ex033.py
@@ -0,0 +1,8 @@
+v = [0, 0, 0]
+
+v[0] = int(input('Primeiro valor: '))
+v[1] = int(input('Segundo valor: '))
+v[2] = int(input('Terceiro valor: '))
+
+print(f'O menor valor é {min(v)}')
+print(f'O maior valor é {max(v)}')
