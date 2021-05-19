4c789b138be2ec5ff75da939e0b0f0a3bcf39245
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-05-19 18:13:34 -0300

    Exercício 37 feito

---
+++ Mundo 2/ex036.py
@@ -0,0 +1,16 @@
+n = int(input('Digite um número: '))
+
+escolha = int(input('''Escolha uma base para converter o número
+[ 1 ] Binário
+[ 2 ] Octal
+[ 3 ] Exadecimal
+Opção: '''))
+
+if escolha == 1:
+	print(f'O número {n} em binário é {n:b}')
+elif escolha == 2:
+	print(f'O número {n} em octal é {n:o}')
+elif escolha == 3:
+	print(f'O número {n} em exadecimal é {n:x}')
+else:
+	print('Escolha INVÁLIDA!')
