73103013945f597ca8bd16a0f295e41bbc9d0cd1
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-05-20 14:34:46 -0300

    Exercício 39 feito

---
+++ Mundo 2/ex039.py
@@ -0,0 +1,15 @@
+from datetime import date
+anoAtual = date.today().year
+
+anoNascimento = int(input('Ano de nascimento: '))
+idade = anoAtual - anoNascimento
+print(f'Quem nasceu em {anoNascimento} tem {idade} anos em {anoAtual}.')
+
+if idade < 18:
+	print(f'Faltam {18 - idade} anos para se alistar.')
+	print(f'Alistamento em {anoAtual + (18 - idade)}')
+elif idade > 18:
+	print(f'Já deveria ter se alistado há {idade - 18} anos.')
+	print(f'O alistamento foi em {anoAtual - (idade - 18)}.')
+elif idade == 18:
+	print("Deve se alistar imediatamete!")
