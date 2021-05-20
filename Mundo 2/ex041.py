130ffbbff90d5c29b6d38cf6c6e1e6b6cb17c80c
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-05-20 19:23:43 -0300

    Exercício 41 feito

---
+++ Mundo 2/ex041.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+anoAtual = date.today().year
+anoNascimeno = int(input('Digite o ano de nascimento do atleta: '))
+idade = anoAtual - anoNascimeno
+
+if idade >= 6:
+	if idade <= 9:
+		print(f'Com {idade} anos o atleta é MIRIM')
+	elif idade <= 14:
+		print(f'Com {idade} anos o atleta é INFANTIL')
+	elif idade <= 19:
+		print(f'Com {idade} anos o atleta é JUNIOR')
+	elif idade <= 25:
+		print(f'Com {idade} anos o atleta é SÊNIOR')
+	else:
+		print(f'Com {idade} anos o atleta é MASTER')
+else:
+	print("Idade Inválida!\nIdade mínima de 6 anos!")
