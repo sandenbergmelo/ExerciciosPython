e50b4e2aa8b8023ad7d5c0101061f95423c11d14
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-08-23 16:59:26 -0300

    Exercício 62 feito

---
+++ Mundo 2/ex062.py
@@ -0,0 +1,21 @@
+print('Gerador de PA')
+print('-=' * 10)
+
+a1 = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+
+total = 0
+mais = 10
+
+i = 1
+while mais > 0:
+	total += mais
+	while i <= total:
+		print(f'{a1} → ', end='')
+		a1 += r
+		i += 1
+	else:
+		print('PAUSA')
+	mais = int(input('Mais quantos termos? '))
+else:
+	print(f'Progressão finalizada com {total} termos')
