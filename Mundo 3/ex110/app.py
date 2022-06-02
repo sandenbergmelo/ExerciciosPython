0486846de5d04457265410efc424d54e528b477f
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-06-02 17:43:16 -0300

    Exercício 110 feito

---
+++ Mundo 3/ex110/app.py
@@ -0,0 +1,7 @@
+# Formatando Moedas em Python
+
+import moeda
+
+p = float(input('Digite um preço R$'))
+
+moeda.resumo(p)
--- Mundo 3/ex110/moeda.py
+++ Mundo 3/ex110/moeda.py
@@ -1,3 +1,18 @@
+def resumo(valor=0, aumento=10, redução=5):
+    """Mostra um resumo do valor"""
+    print('-'*35)
+    print('RESUMO DO VALOR'.center(35))
+    print('-'*35)
+
+    print(f'Preço analisado: \t{formatar_moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor, True)}')
+    print(f'Metade do preço: \t{metade(valor, True)}')
+    print(f'Aumento de {aumento}%: \t{aumentar(valor, aumento, True)}')
+    print(f'Redução de {redução}%: \t\t{reduzir(valor, redução, True)}')
+
+    print('-'*35)
+
+
 def aumentar(valor=0, taxa=0, formatar=False):
     """Retorna o valor com uma aumento percentual"""
     res = valor * (100 + taxa) / 100
@@ -6,8 +21,8 @@ def aumentar(valor=0, taxa=0, formatar=False):
     return res
 
 
-def diminuir(valor=0, taxa=0, formatar=False):
-    """Retorna o valor com uma diminuição percentual"""
+def reduzir(valor=0, taxa=0, formatar=False):
+    """Retorna o valor com uma redução percentual"""
     res = valor * (100 - taxa) / 100
     if formatar:
         return formatar_moeda(res)
