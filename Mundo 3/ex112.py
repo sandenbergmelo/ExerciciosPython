fc8b476adb8785189584561723b07999b3d9f517
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-06-02 18:30:33 -0300

    Exercício 112 feito

---
+++ Mundo 3/ex112.py
@@ -0,0 +1,7 @@
+# Entrada de dados monetários
+
+from utilidadesCeV import moeda, dado
+
+p = dado.ler_dinheiro('Digite um valor R$: ')
+
+moeda.resumo(p)
---
+++ Mundo 3/utilidadesCeV/dado/__init__.py
@@ -0,0 +1,26 @@
+from rich import print
+
+
+def ler_dinheiro(msg: str = '') -> float:
+    """Lê o um número real no formato monetário"""
+    while True:
+        valor = str(input(msg)).strip().replace(',', '.')
+
+        if valor.isalpha() or valor == '':
+            print(f'[red][bold]ERRO![/] \"{valor}\" é um valor inválido![/]')
+            continue
+
+        return float(valor)
+
+
+def ler_int(msg: str = '') -> int:
+    """Lê um número inteiro"""
+    from math import floor
+    while True:
+        valor = str(input(msg)).strip()
+
+        if not valor.isdigit() or valor == '':
+            print('[red][bold]ERRO![/] Digite um número inteiro válido[/]')
+            continue
+
+        return floor(float(valor))
