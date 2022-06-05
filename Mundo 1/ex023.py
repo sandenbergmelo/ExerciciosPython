4e5555b9a4135923552b09d30db56ff76135a76f
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-06-05 19:58:42 -0300

    Update ex023

--- Mundo 1/ex023.py
+++ Mundo 1/ex023.py
@@ -1,11 +1,11 @@
 n = int(input('Digite um número: '))
 
-u = n % 10 
+u = n % 10
 d = n // 10 % 10
 c = n // 100 % 10
 m = n // 1000 %10
 
-print(f'Unidande: {u}')
+print(f'Unidade: {u}')
 print(f'Dezena: {d}')
 print(f'Centena: {c}')
 print(f'Milhar: {m}')
